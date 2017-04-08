87fb4e66a3e71c17d5484c9dce4d72665c34308b
Goran Milošević <goran.milosevic@inducta.local>
2017-04-08 10:47:04 +0200

    initial gui version

---
+++ 3_gui/3_gui/3_gui/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_3_gui.Startup))]
+namespace _3_gui
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
